71f48291f29847397b3895bc3301ee1abababafe
Jacco <jaccobroeren@freedom.nl>
2022-04-30 18:51:06 +0200

    19/2/2022 1

--- Anonimize/db.py
+++ Anonimize/db.py
@@ -1,13 +1,26 @@
-
 import mysql.connector
+from mysql.connector import Error
 
 def new_db_connection():
-    connection = mysql.connector.connect(
-        user='jbroeren',
-        password='Hello123!!*',
-        host='127.0.0.1',
-        port=3306,
-        database='restdb'
-    )
+
+    try:
+
+        connection = mysql.connector.connect(
+            user='jbroeren',
+            password='Hello123!!*',
+            host='127.0.0.1',
+            port=3306,
+            database='restdb'
+        )
+
+        if connection.is_connected():
+            print('Connected to MySQL database')
+
+    except Error as e:
+        print(e)
+
+    finally:
+        if connection is not None and connection.is_connected():
+            connection.close()
 
     return connection
