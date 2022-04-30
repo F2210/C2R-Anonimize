a45a110e591ac0bd0c3be6387c4978062586f5ed
Jacco <jaccobroeren@freedom.nl>
2022-04-30 20:24:27 +0200

    19/2/2022 1

--- Anonimize/db.py
+++ Anonimize/db.py
@@ -8,7 +8,7 @@ def new_db_connection():
         connection = mysql.connector.connect(
             user='jbroeren',
             password='Hello123!!*',
-            host='127.0.0.1',
+            host='localhost',
             port=3306,
             database='restdb'
         )
