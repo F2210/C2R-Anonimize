f73083c6bdb1085a868cc8808f292f4de9dfef1e
Jacco <jaccobroeren@freedom.nl>
2022-04-30 20:47:23 +0200

    19/2/2022 1

--- Anonimize/runner.py
+++ Anonimize/runner.py
@@ -11,6 +11,22 @@ from Anonimize.controller import de_identify
 
 connection = new_db_connection()
 
+def syllable_count(sentence):
+    count = 0
+    vowels = "aeiouy"
+    for word in sentence.split(" "):
+        if word != "":
+            if word[0] in vowels:
+                count += 1
+            for index in range(1, len(word)):
+                if word[index] in vowels and word[index - 1] not in vowels:
+                    count += 1
+                    if word.endswith("e"):
+                        count -= 1
+            if count == 0:
+                count += 1
+    return count
+
 def main():
 
     if not connection.is_connected():
@@ -42,6 +58,9 @@ def main():
                 )
 
                 session = dict(zip([column[0] for column in c.description], c.fetchone()))
+
+            # if syllable_count(result["original_text"]) < 12:
+
 
             textdata = {
                 "id": result["id"],
