9f18f5e8ad0511f6814bcfe3a5c9b7939af3ab58
Jacco <jaccobroeren@freedom.nl>
2022-04-30 22:39:02 +0200

    19/2/2022 1

--- Anonimize/models.py
+++ Anonimize/models.py
@@ -1,6 +1,6 @@
 nermodels = {
-    "nl": ("wietsedv/bert-base-multilingual-cased-finetuned-udlassy-ner", "transformer", "MAIN/SNOMEDCT-NL", "2021-09-30"),
-    # "nl": ("GroNLP/gpt2-small-dutch", "flair", "MAIN/SNOMEDCT-NL", "2021-09-30"),
+    # "nl": ("wietsedv/bert-base-multilingual-cased-finetuned-udlassy-ner", "transformer", "MAIN/SNOMEDCT-NL", "2021-09-30"),
+    "nl": ("GroNLP/gpt2-small-dutch", "flair", "MAIN/SNOMEDCT-NL", "2021-09-30"),
     # "en": ("dslim/bert-base-NER", "transformer", "MAIN/SNOMEDCT-US", "2021-09-01"),
     # "en": ("distilgpt2", "flair", "MAIN/SNOMEDCT-US", "2021-09-01"),
 }
