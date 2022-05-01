9429cd02a86b7aaf6b2ab78ded0e036b89c41436
Jacco <jaccobroeren@freedom.nl>
2022-05-01 15:00:50 +0200

    19/2/2022 1

---
+++ Anonimize/primer.py
@@ -0,0 +1,27 @@
+import datetime, requests
+from time import sleep
+
+
+def main():
+
+    primersesh = requests.post("http://192.168.1.175:8001/sessiondata?type=open",
+                          json={"sessionID": str(datetime.datetime.now().timestamp() + 4)}).json()["response_data"]["ID"]
+
+    try:
+        requests.post(
+            "http://192.168.1.175:8001/textdedata",
+            json={
+                "text": "Dit primer tekst.",
+                "sessionID": primersesh
+            },
+            timeout=0.000000001)
+
+    except requests.exceptions.ReadTimeout:
+        pass
+    except requests.exceptions.ConnectTimeout:
+        pass
+
+while True:
+    main()
+    print("priming")
+    sleep(2)
